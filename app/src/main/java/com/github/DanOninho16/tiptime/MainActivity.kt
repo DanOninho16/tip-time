f2a14e704bb08cdf14c978ca39b5ce6c43b56f8d
DanOninho16 <dan22cardoso@gmail.com>
2023-08-07 14:26:54 -0300

    First commit

---
+++ app/src/main/java/com/github/DanOninho16/tiptime/MainActivity.kt
@@ -0,0 +1,11 @@
+\package com.github.DanOninho16.tiptime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
